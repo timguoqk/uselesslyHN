d41802f55517f751340aa53bd618e96c9d43aa3f
Tim Guo <timguoqk@gmail.com>
2014-12-08 13:45:43 -0800

    basic framework

---
+++ js/MLWorker.js
@@ -0,0 +1,9 @@
+//http://www.html5rocks.com/en/tutorials/workers/basics/#toc-enviornment-subworkers
+
+var MLWorker = function(){
+	self.addEventListener("");
+}
+
+MLWorker.prototype.method_name = function(first_argument) {
+	// body...
+};
---
+++ js/popup.js
@@ -0,0 +1,6 @@
+//http://www.html5rocks.com/en/tutorials/workers/basics/#toc-enviornment-subworkers
+var worker = new Worker('js/predictionWorker.js');
+
+document.addEventListener('DOMContentLoaded', function(){
+
+});
